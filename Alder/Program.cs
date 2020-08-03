aafad389d83ac19450ee858601b45d0791e3bee0
cami670v <cami670v@ZBC-AA-UBUY12>
2020-08-03 13:38:55 +0200

    Add project files.

---
+++ Alder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Alder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string navn = Console.ReadLine();
+            int alder = int.Parse(Console.ReadLine());
+        }
+    }
+}
